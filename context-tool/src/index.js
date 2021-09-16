31ba280c1f0c08d5669f2618328d3917c024073c
Deborah Ong <deborah.oyt@gmail.com>
2021-09-15 19:59:33 -0700

    add context-tool

---
+++ context-tool/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import ThemeContext from './themeContext'
+
+ReactDOM.render(
+  <React.StrictMode>
+  <ThemeContext.Provider value={'light'}>
+      <App />
+  </ThemeContext.Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
