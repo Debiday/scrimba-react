daf683fdbe575fda18a99fff34fe9c0db76bd810
Deborah Ong <deborah.oyt@gmail.com>
2021-09-25 17:45:01 -0700

    add custom hook

--- speed-typing-game/src/App.js
+++ speed-typing-game/src/App.js
@@ -1,67 +1,27 @@
 import React, {useState, useEffect, useRef} from 'react'
 import './App.css'
+import useWordGame from './useWordGame'
 
 function App() {
-  const STARTING_TIME = 5
+  const {textBoxRef, handleChange, text, isTimeRunning, timeRemaining, startGame, wordCount} = useWordGame()
+  return (
+      <div>
+          <h1>How fast do you type?</h1>
+          <textarea
+              ref={textBoxRef}
+              onChange={handleChange}
+              value={text}
+              disabled={!isTimeRunning}
+          />
+          <h4>Time remaining: {timeRemaining}</h4>
+          <button 
+            onClick={startGame}
 
-  const [text, setText] = useState("")
-  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
-  const [isTimeRunning, setIsTimeRunning] = useState(false)
-  const [wordCount, setWordCount] = useState(0)
-  const textBoxRef = useRef(null)
-
-  function handleChange(event) {
-    const {value} = event.target
-    setText(value)  
+          >
+          Start</button>
+          <h1>Word count: {wordCount}</h1>
+      </div>
+  )
   }
-  
-  function calculateWordCount(text) {
-    const wordsArr = text.trim().split(" ")
-    return wordsArr.filter(word => word !== "").length
-}
-
-  function startGame() {
-    setIsTimeRunning(true)
-    setText("")
-    setTimeRemaining(STARTING_TIME)
-    textBoxRef.current.disabled = false
-    textBoxRef.current.focus()
-  }
-
-  function endGame() {
-    setIsTimeRunning(false)
-    setWordCount(calculateWordCount(text))
-    console.log("wordCount", wordCount)
-  }
-
-  useEffect(() => {
-    if(isTimeRunning && timeRemaining > 0) {
-        setTimeout(() => {
-            setTimeRemaining(time => time - 1)
-        }, 1000)
-    } else if(timeRemaining === 0) {
-        endGame()
-    }
-}, [timeRemaining, isTimeRunning]) // eslint-disable-line react-hooks/exhaustive-deps
-
-return (
-    <div>
-        <h1>How fast do you type?</h1>
-        <textarea
-            ref={textBoxRef}
-            onChange={handleChange}
-            value={text}
-            disabled={!isTimeRunning}
-        />
-        <h4>Time remaining: {timeRemaining}</h4>
-        <button 
-          onClick={startGame}
-
-        >
-        Start</button>
-        <h1>Word count: {wordCount}</h1>
-    </div>
-)
-}
 
 export default App
---
+++ speed-typing-game/src/useWordGame.js
@@ -0,0 +1,49 @@
+import {useState, useEffect, useRef} from 'react'
+
+function useWordGame() {
+    const STARTING_TIME = 5
+    
+    const [text, setText] = useState("")
+    const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
+    const [isTimeRunning, setIsTimeRunning] = useState(false)
+    const [wordCount, setWordCount] = useState(0)
+    const textBoxRef = useRef(null)
+    
+    function handleChange(event) {
+        const {value} = event.target
+        setText(value)  
+    }
+    
+    function calculateWordCount(text) {
+        const wordsArr = text.trim().split(" ")
+        return wordsArr.filter(word => word !== "").length
+    }
+    
+    function startGame() {
+        setIsTimeRunning(true)
+        setText("")
+        setTimeRemaining(STARTING_TIME)
+        textBoxRef.current.disabled = false
+        textBoxRef.current.focus()
+    }
+    
+    function endGame() {
+        setIsTimeRunning(false)
+        setWordCount(calculateWordCount(text))
+        console.log("wordCount", wordCount)
+    }
+    
+    useEffect(() => {
+        if(isTimeRunning && timeRemaining > 0) {
+            setTimeout(() => {
+                setTimeRemaining(time => time - 1)
+            }, 1000)
+        } else if(timeRemaining === 0) {
+            endGame()
+        }
+    }, [timeRemaining, isTimeRunning]) // eslint-disable-line react-hooks/exhaustive-deps
+    
+    return {textBoxRef, handleChange, text, isTimeRunning, timeRemaining, startGame, wordCount}
+}
+
+export default useWordGame
