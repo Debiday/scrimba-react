0bd2b14b2d4ef1af2cd9642fe8518e51fc6fe6b4
Deborah Ong <deborah.oyt@gmail.com>
2021-10-06 09:28:29 -0700

    indicate carted icon

--- pic-shop/src/Context.js
+++ pic-shop/src/Context.js
@@ -35,7 +35,7 @@ function ContextProvider({children}) {
     console.log(cartItems)
 
     return (
-        <Context.Provider value={{allPhotos, toggleFavorite, addToCart}}>
+        <Context.Provider value={{allPhotos, toggleFavorite, addToCart, cartItems}}>
             {children}
         </Context.Provider>
     )
--- pic-shop/src/components/Image.js
+++ pic-shop/src/components/Image.js
@@ -6,7 +6,7 @@ function Image({className, img}) {
 
     const [hovered, setHovered] = useState(false)
     
-    const {toggleFavorite, addToCart} = useContext(Context)
+    const {toggleFavorite, addToCart, cartItems} = useContext(Context)
 
     // const heartIcon = hovered && 
     //     <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
@@ -19,8 +19,19 @@ function Image({className, img}) {
         }
     }
 
-    const cartIcon = hovered && 
-        <i className="ri-add-circle-line cart" onClick={() => addToCart(img)}></i>
+    function cartIcon() {
+        const alreadyInCart = cartItems.some(items => items.id === img.id)
+        if (alreadyInCart) {
+            return <i className="ri-shopping-cart-fill cart"></i>
+        }
+        else if(hovered) {
+            return <i className="ri-add-circle-line cart" onClick={() => addToCart(img)}></i>
+        }
+    }
+    
+
+    // const cartIcon = hovered && 
+    //     <i className="ri-add-circle-line cart" onClick={() => addToCart(img)}></i>
 
     // console.log(hovered)
 
@@ -32,10 +43,11 @@ function Image({className, img}) {
         >
             <img src={img.url} className='image-grid' alt=''/>
             {heartIcon()}
-            {cartIcon}
+            {cartIcon()}
         </div>
     )
 }
+
 
 Image.propTypes = {
     className: Proptypes.string,
