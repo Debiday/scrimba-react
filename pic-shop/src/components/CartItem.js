a84da1ae3a22724f9f2658724b825ec72963799c
Deborah Ong <deborah.oyt@gmail.com>
2021-10-06 22:26:43 -0700

    trash icon hover function

--- pic-shop/src/components/CartItem.js
+++ pic-shop/src/components/CartItem.js
@@ -1,12 +1,21 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import {Context} from '../Context'
 
 function CartItem({item}) {
     const {removeFromCart} = useContext(Context)
+    const [trashHovered, setTrashHovered] = useState(false)
+
+    const iconClassName = trashHovered ? 'ri-delete-bin-fill' : 'ri-delete-bin-line'
 
     return (
         <div className='cart-item'>
-            <i className='ri-delete-bin-line' onClick={() => removeFromCart(item.id)}></i>
+            <i 
+                className={iconClassName}
+                onClick={() => removeFromCart(item.id)}
+                onMouseEnter={() => setTrashHovered(true)}
+                onMouseLeave={() => setTrashHovered(false)}
+            >
+            </i>
             <img src={item.url} width='130px' alt=''/>
             <p>$5.99</p>
         </div>
